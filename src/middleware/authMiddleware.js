ceaf61f5a89dca477266f3eea071882fa515bb3a
Ashraf <nayeemashraf92@gmail.com>
2026-06-19 16:51:42 +0500

    add roleMiddleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -18,11 +18,3 @@ export const protect = (req, res, next) => {
   }
 };
 
-export const authorize = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ message: `Access denied. Required role: ${roles.join(', ')}` });
-    }
-    next();
-  };
-};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { register, login } from '../controller/user.controller.js';
-import { protect, authorize } from '../middleware/authMiddleware.js';
+import { protect } from '../middleware/authMiddleware.js';
+import { authorize } from '../middleware/roleMiddleware.js';
 
 const router = express.Router();
 
